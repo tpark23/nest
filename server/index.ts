0673015790550730c8df2181705b32430e10f310
Thomas <tpark229@gmail.com>
2024-12-11 14:50:05 -0500

    Add TypeScript support

---
+++ server/index.ts
@@ -0,0 +1,19 @@
+// backend/index.ts
+// backend/index.ts
+
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
+});
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World!");
+});
+
+app.get("/api", (req: Request, res: Response) => {
+    res.json({ message: "Hello World! I'm JSON" });
+});
