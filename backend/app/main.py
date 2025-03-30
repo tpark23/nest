07aafcf7dce451bda6d675528f29f4f59b64a88d
Thomas <tpark229@gmail.com>
2025-03-29 22:59:46 -0400

    feat: setup default route

---
+++ backend/app/main.py
@@ -0,0 +1,12 @@
+import uvicorn
+from fastapi import FastAPI, APIRouter
+
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World!"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
